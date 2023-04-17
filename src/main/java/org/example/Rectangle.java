72b6dca827d403877e6608d1d03d9ec7324068f9
sabato-nocera <nocera.sabato@gmail.com>
2023-04-17 14:43:39 +0200

    Initial commit

---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Rectangle {
+    private final int length;
+    private final int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimeter() {
+        return (length + width) * (length + width);
+    }
+}
---
+++ src/test/java/org/example/RectangleTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RectangleTest {
+
+    Rectangle rectangle;
+
+    @BeforeEach
+    void setUp() {
+        rectangle = new Rectangle(5, 10);
+    }
+
+    @Test
+    void testGetLength() {
+        assertEquals(5, rectangle.getLength());
+    }
+
+    @Test
+    void testGetWidth() {
+        assertEquals(10, rectangle.getWidth());
+    }
+
+    @Test
+    void testGetArea() {
+        assertEquals(50, rectangle.getArea());
+    }
+
+    @Test
+    void testGetPerimeter() {
+        assertEquals(30, rectangle.getPerimeter());
+    }
+}
